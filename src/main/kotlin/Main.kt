1fad6f64234db6bae30dfc3c99fea2e4a64fe298
Duarte Cancela <duartecancela@gmail.com>
2021-10-06 23:04:24 +0100

    implement save to file and create pdf

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,21 +6,38 @@ fun main(args: Array<String>) {
     // read from file
     val lines = File("dep-pl.txt").readLines()
 
-    val delimit = ","
+    // output filename
+    var outFilename = "final.dot"
+
+    val delimit = ", "
+
+    // write first line of output file
+    File(outFilename).writeText("digraph ProgramingLanguages { \n")
 
     // process each line
     for (line in lines) {
-        println("$line")
+        //println("$line")
 
-        val parts = line.split(delimit)
-        println(parts)
-        println(parts.size)
+        val parts = line.split(delimit) // create string array from each line
+        //println(parts) // show array of strings splitted by ", "
+        //println(parts.size)
+        val partsSize = parts.size // size of strings array
 
-        for (i in parts) {
-            println(i)
+        var count = 0
+        for (i in parts) { // iterate array of strings
+            if (count != 0) {
+                //println("\"" + i + "\"" + " -> " + "\"" + parts[0] + "\"" + ";")
+                File(outFilename).appendText("\"" + i + "\"" + " -> " + "\"" + parts[0] + "\"" + ";\n")
+            }
+            count += 1
+            println(count)
         }
-    }
-    // write file
-    //File("fileName").writeText("fileContent")
 
+    }
+
+    // write last line of file
+    File(outFilename).appendText("}")
+
+    // execute dot
+    Runtime.getRuntime().exec("dot -Tpdf $outFilename -o out.pdf")
 }
