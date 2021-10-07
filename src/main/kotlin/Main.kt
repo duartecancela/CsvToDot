534960bf1f219380c9c79a103caeb0b505fa5ba7
Duarte Cancela <duartecancela@gmail.com>
2021-10-07 15:49:43 +0100

    finish code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,18 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("Kotlin program! \n")
-
-    // read from file
-    val lines = File("dep-pl.txt").readLines()
+    println("Kotlin program to convert CSV to DOT \n")
 
     // output filename
-    var outFilename = "final.dot"
+    val outFilename = "final.dot"
 
+    // input filename
+    val inFilename = "dep-pl.txt"
+
+    // read from file
+    val lines = File(inFilename).readLines()
+
+    // String delimiter
     val delimit = ", "
 
     // write first line of output file
@@ -16,26 +20,25 @@ fun main(args: Array<String>) {
 
     // process each line
     for (line in lines) {
-        //println("$line")
+        //println("$line") // debug
 
         val parts = line.split(delimit) // create string array from each line
-        //println(parts) // show array of strings splitted by ", "
-        //println(parts.size)
+        //println(parts) // show array of strings splitted by ", " // debug
+        //println(parts.size) // debug
         val partsSize = parts.size // size of strings array
 
         var count = 0
         for (i in parts) { // iterate array of strings
             if (count != 0) {
-                //println("\"" + i + "\"" + " -> " + "\"" + parts[0] + "\"" + ";")
+                //println("\"" + i + "\"" + " -> " + "\"" + parts[0] + "\"" + ";") // debug
                 File(outFilename).appendText("\"" + i + "\"" + " -> " + "\"" + parts[0] + "\"" + ";\n")
             }
             count += 1
-            println(count)
+            //println(count) // debug
         }
-
     }
 
-    // write last line of file
+    // write last line of output file
     File(outFilename).appendText("}")
 
     // execute dot
